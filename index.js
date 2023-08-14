2217c08a57407c67610713fafdbe6e7459b9fd9b
realguystuff <101049350+realguystuff@users.noreply.github.com>
2023-08-14 10:58:21 +0000

    index updaate

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 require('./server.js');
 
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { clientId, guildId } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.token);
